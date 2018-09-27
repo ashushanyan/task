83c05ba1d10a7d5d1ab8bc2d046be51508918fee
Ivan Pilikov <ivan.pilikov@gmail.com>
2018-09-28 01:31:46 +0400

    search

--- app/Http/DataProviders/Post/IndexDataProvider.php
+++ app/Http/DataProviders/Post/IndexDataProvider.php
@@ -5,24 +5,37 @@ namespace App\Http\DataProviders\Post;
 
 use App\Http\DataProviders\AbstractDataProvider;
 use App\Http\Filters\PostFilter;
+use Illuminate\Http\Request;
 use App\Post;
 use App\Tag;
 
 class IndexDataProvider extends AbstractDataProvider
 {
-    private $filter;
+    private $request;
 
-    public function __construct(PostFilter $filter)
+    public function __construct(Request $request)
     {
-        $this->filter = $filter;
+        $this->request = $request;
     }
 
     public function getData()
     {
-        return Post::with('user', 'tags')
-                ->filterUsing($this->filter)
-                ->latest()
-                ->get();
+        return  Post::with('user', 'tags')
+                    ->where('title', 'like', '%'.$this->request['search_key'].'%')
+                    ->whereHas('tags', $this->filterByTags())
+                    ->latest()
+                    ->get();
+    }
+
+    public function filterByTags() 
+    {
+        return function ($query) {
+                if(empty($this->request['tag_ids'])) {
+                    return null;
+                } else {
+                    $query->whereIn('tag_id', $this->request['tag_ids']);
+                }
+            };
     }
 
 }
